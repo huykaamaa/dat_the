0fde430cac81e4a32728fe0d711b37ca2f51d862
huykaamaa <iwbrhwfy@gmail.com>
2026-08-05 19:46:09 +0700

    sua chan final

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ unsigned long heartbeatInterval = 60000; // ms, 0 = tat
 //================ SENSOR / RELAY / TRIGGER ================
 
 const uint8_t sensorPins[SENSOR_NUM] = {39, 40, 42, 2, 1}; 
-const uint8_t relayPins[SENSOR_NUM] = {4, 5, 6, 7, 15};
+const uint8_t relayPins[SENSOR_NUM] = {15, 7, 6, 5, 4}; //{4, 5, 6, 7, 15}; 
 
 bool sensorEnable[SENSOR_NUM] = {true, true, true, true, true};
 bool relayState[SENSOR_NUM] = {0};
